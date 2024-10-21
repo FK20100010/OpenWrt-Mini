0a8ca1426f449341b9b91abc4e14a8306989d595
FK20100010 <154255559+FK20100010@users.noreply.github.com>
2024-10-22 04:54:44 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -15,19 +15,19 @@ sed -i 's/192.168.1.1/192.168.10.100/g' package/base-files/files/bin/config_gene
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
 # 5-修改时间格式
-sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
+#sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' feeds/kwrt_packages/autocore/files/*/index.htm
 
 # 6-添加固件日期
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
-sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
+#sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
+#sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
 
 # 7-修正硬件信息
-#sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' feeds/kwrt-packages/autocore/files/autocore
+sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' feeds/kwrt_packages/autocore/files/autocore
 
 # 8-增固件连接数
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
-./scripts/feeds install -a -p kwrt-packages -f
+./scripts/feeds install -a -p kwrt_packages -f
 rm -rf feeds/packages/net/chinadns-ng
-cp -rf feeds/kwrt-packages/chinadns-ng/ feeds/packages/net/
+cp -rf feeds/kwrt_packages/chinadns-ng/ feeds/packages/net/
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
