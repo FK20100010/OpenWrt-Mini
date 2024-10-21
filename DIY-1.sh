336283d732b432eaf811ea6a506538c266b0b2e1
FK20100010 <154255559+FK20100010@users.noreply.github.com>
2024-10-22 03:17:35 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -18,6 +18,12 @@
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
+# 6-添加 kwrt-packages插件
+echo "src-git kwrt-packages https://github.com/kiddin9/kwrt-packages.git;main" >> "feeds.conf.default"
+
+rm -rf feeds/packages/net/chinadns-ng
+cp -rf feeds/kwrt-packages/chinadns-ng/ feeds/packages/net/
+
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
