e2e8cab934bfcd2485afdfbd6b4594fb6e3e7773
FK20100010 <154255559+FK20100010@users.noreply.github.com>
2024-10-23 00:18:15 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -8,11 +8,12 @@
 # 2-添加 PowerOff 关机插件
 #git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
 
-# 3-添加 opentomcat 主题
-#git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
+# 3-添加主题
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone -b js --single-branch https://github.com/papagaye744/luci-theme-design.git package/luci-theme-design
 
-# 4-添加 OpenClash 插件
-#sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
+# 4-添加 clouddrive2 插件
+git clone https://github.com/kiddin9/openwrt-clouddrive2.git package/openwrt-clouddrive2
 
 # 5-添加 PassWall 插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
