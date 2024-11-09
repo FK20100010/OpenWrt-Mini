7f18899bb8f3241323db7d8bb6827151490963e5
FK20100010 <154255559+FK20100010@users.noreply.github.com>
2024-11-09 21:15:09 +0800

    Update build.sh

--- additional/build.sh
+++ additional/build.sh
@@ -7,10 +7,21 @@ BASE_PATH=$(cd $(dirname $0) && pwd)
 BUILD_DIR=$(cat BUILD_DIR)
 BUILD_MODEL=$(cat BUILD_MODEL)
 CONFIG_FILE=$BASE_PATH/additional/$BUILD_MODEL.config
-\cp -f $BASE_PATH/additional/$BUILD_MODEL.config $BASE_PATH/$BUILD_DIR/.config
+
+if [[ ! -f "$CONFIG_FILE" ]]; then
+    aa=$(grep -lri $BUILD_MODEL $BASE_PATH$BUILD_DIR/target | awk -F'[/.]' '{print $3}')
+    bb=$(grep -lri $BUILD_MODEL $BASE_PATH$BUILD_DIR/target | awk -F'[/.]' '{print $5}')
+    cat>$BASE_PATH/$BUILD_DIR/.config<<EOF
+    CONFIG_TARGET_$aa=y
+    CONFIG_TARGET_$aa_$bb=y
+    CONFIG_TARGET_$aa_$bb_DEVICE_$BUILD_MODEL=y
+    EOF
+    else
+    \cp -f $BASE_PATH/additional/$BUILD_MODEL.config $BASE_PATH/$BUILD_DIR/.config
+fi
 
 DEVICE_NAME=$(grep '^CONFIG_TARGET.*DEVICE.*=y' $CONFIG_FILE | sed -r 's/.*DEVICE_(.*)=y/\1/')
-if [[ "$DEVICE_NAME" != "jdcloud_ax1800-pro" ]]; then
+if [[ "$DEVICE_NAME" != "jdcloud_ax1800-pro" ]] || [[ "$DEVICE_NAME" != "jdcloud_re-ss-01" ]]; then
     sed -i "s/FK20100010/$DEVICE_NAME/g" $BASE_PATH/additional/99-additional-settings
     sed -i '/.encryption=/d' $BASE_PATH/additional/99-additional-settings
     sed -i '/.key=/d' $BASE_PATH/additional/99-additional-settings
