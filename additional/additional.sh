2303b746c40671116108dd4d19d26bba96c6879a
FK20100010 <154255559+FK20100010@users.noreply.github.com>
2024-11-11 11:00:58 +0800

    Update additional.sh

--- additional/additional.sh
+++ additional/additional.sh
@@ -7,7 +7,7 @@ if [[ -f "additional/$BUILD_MODEL.sh" ]]; then
     additional/$BUILD_MODEL.sh
 fi
 
-cd 
+cd $BUILD_DIR
 sed -i '/^#/d' feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
