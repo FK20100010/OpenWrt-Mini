f047ff1ca733512d3a635438533c84e80ce4c900
FK20100010 <154255559+FK20100010@users.noreply.github.com>
2024-12-13 23:07:37 +0800

    Update additional.sh

--- additional/additional.sh
+++ additional/additional.sh
@@ -38,5 +38,5 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' packag
 #rm -rf feeds/luci/applications/luci-app-dockerman
 #git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 git clone https://github.com/coolsnowwolf/lede
-rm -rf feeds/nss_packages/qca-nss-ecm
-cp -rf lede/package/qca/qca-nss-ecm feeds/nss_packages/
+rm -rf feeds/nss_packages/
+cp -rf lede/package/qca feeds/nss_packages
